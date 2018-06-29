36addbc02abbb7aea1532422d2a1ef3ca2d0cce3
Hassan Alhuzali <hhass2009@hotmail.com>
2018-06-29 07:16:40 -0700

    Update loading_data.py

--- codes/loading_data.py
+++ codes/loading_data.py
@@ -11,18 +11,6 @@ import numpy as np
 import re
 import csv
 
-
-def tokenizedTweet(tweet):
-    """
-
-    :param tweet: str()
-    :return: a tokenized tweet
-    """
-    from tweetokenize import Tokenizer
-    tokens = Tokenizer(lowercase=True, normalize=2, allcapskeep=False, ignorestopwords=True)
-    tokenize= tokens.tokenize(tweet)
-    return ' '.join(tokenize)
-
 def loadingData(filename):
     """return a list of tweets and their labels
 
@@ -32,10 +20,7 @@ def loadingData(filename):
     df = pd.concat(df, ignore_index=True)
     df.columns = ['label', 'tweet']
     print('...Cleaning Data...')
-    # filter non-Arabic chars, convert @name to "<USER>", conver http to "<URL>", keep a tweet with more than 5 words at min
     df['tweet'].replace(to_replace="\s+", value=r" ", regex=True, inplace=True)
-    # df = df[df.tweet.str.split().str.len() >= 5]
-    #df['tweet']= df.apply(lambda row: tokenizedTweet(row['tweet']), axis=1)
     df['tweet'] = df.apply(lambda row: row['tweet'].lower().strip().strip('"'), axis=1)
     df['label'] = df.apply(lambda row: row['label'].lower().strip().strip('"'), axis=1)
 
